f3f40c379f18a9315b049f40bd7f1f9912c5b9b7
rioka <rioka@ASUS-GL552>
2021-02-04 21:22:24 +0000

    Add nuget.config Add sample consumer

---
+++ TestPackageConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestPackageConsumer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var t = new TestPackage.Foo();
+      t.Bar();
+    }
+  }
+}
