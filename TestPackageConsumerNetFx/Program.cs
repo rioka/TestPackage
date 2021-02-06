602f21f919fb1d805f9c2e0cc895e2d6e128a499
rioka <rioka@ASUS-GL552>
2021-02-06 00:27:15 +0000

    Add sample NET fx consumer

---
+++ TestPackageConsumerNetFx/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using Newtonsoft.Json;
+
+namespace TestPackageConsumerNetFx
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var t = new TestPackage.Foo();
+      var bar = t.Bar();
+      Console.WriteLine($"Foo.Bar returned {bar}");
+
+      var json = JsonConvert.SerializeObject(new { One = 1 });
+      Console.WriteLine(json);
+
+      var mapper = new MapperConfiguration(cfg => { }).CreateMapper();
+    }
+  }
+}
