e8b5f39d7c633024500065d9500fa991a54256ba
rioka <rioka@ASUS-GL552>
2021-02-06 00:26:37 +0000

    Update version to 1.3.4

--- TestPackage/Foo.cs
+++ TestPackage/Foo.cs
@@ -2,11 +2,19 @@
 
 namespace TestPackage
 {
+  /// <summary>
+  /// Dumb class to test package
+  /// </summary>
   public class Foo
   {
+    /// <summary>
+    /// Stupid method
+    /// </summary>
+    /// <returns>A string.</returns>
     public string Bar()
     {
       var now = DateTime.UtcNow;
+      Console.WriteLine("Yeah, it's me!");
       return $"baz {now}";
     }
   }
